3f1c68582e5d09080cc7dbe88c7e8af15590f811
hsbicalho <hsbicalho@gmail.com>
2022-04-26 15:30:26 -0300

    primeiro commit para PR

--- src/services/api.js
+++ src/services/api.js
@@ -4,4 +4,5 @@ export async function getCategories() {
 
 export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+  // mudança pr
 }
