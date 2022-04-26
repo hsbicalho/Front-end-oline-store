d1e2862906c62baa264ac19fa9fa9917d56a86af
Daniel Vieira <dann-vieira@hotmail.com>
2022-04-26 18:19:18 -0300

    [FIX] refatora requisito 02

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,12 +1,16 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class Search extends Component {
   render() {
     return (
-      <div>
-          <input type="text" name="search" data-testid="home-initial-message" />
-          <p>Digite algum termo de pesquisa ou escolha uma categoria.</p>
+      <div data-testid="home-initial-message">
+        <input
+          type="text"
+          name="search"
+        />
+
+        <p>Digite algum termo de pesquisa ou escolha uma categoria.</p>
       </div>
-    )
+    );
   }
 }
