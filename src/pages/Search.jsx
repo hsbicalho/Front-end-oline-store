1711d6402f76ca64f942649c9dfdb4abb3ab5457
Daniel Vieira <dann-vieira@hotmail.com>
2022-04-26 20:07:09 -0300

    Implementa requisito 02

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,16 +1,58 @@
 import React, { Component } from 'react';
+import { getCategories } from '../services/api';
 
 export default class Search extends Component {
-  render() {
-    return (
-      <div data-testid="home-initial-message">
-        <input
-          type="text"
-          name="search"
-        />
+  constructor() {
+    super();
+    this.state = {
+      categoriesState: [],
+    };
+  }
 
-        <p>Digite algum termo de pesquisa ou escolha uma categoria.</p>
-      </div>
+  async componentDidMount() {
+    const categories = await getCategories();
+    this.setState({
+      categoriesState: categories,
+    });
+  }
+
+  render() {
+    const {
+      categoriesState,
+    } = this.state;
+    return (
+      <>
+        <main data-testid="home-initial-message">
+          <input
+            type="text"
+            name="search"
+          />
+
+          <p>Digite algum termo de pesquisa ou escolha uma categoria.</p>
+        </main>
+
+        <aside>
+          <section>
+            <h3>
+              Categorias:
+            </h3>
+            {
+              categoriesState.map((category) => (
+                <ul
+                  key={ category.id }
+                  data-testid="category"
+                >
+                  <button
+                    type="button"
+                  >
+                    { category.name }
+                  </button>
+                </ul>
+              ))
+            }
+          </section>
+        </aside>
+      </>
     );
   }
 }
